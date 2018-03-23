188e64f7dea3f05f414569969683f69fb5287d02
gaopengzhuang <1468325531@qq.com>
2018-03-23 11:08:10 +0800

    modify buserController to developercontroller

--- src/main/java/cn/appinfodb/controller/DeveloperController.java
+++ src/main/java/cn/appinfodb/controller/DeveloperController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BUserController {
+public class DeveloperController {
 	@RequestMapping("/appSearch")
 	public String appSearch() {
 		
